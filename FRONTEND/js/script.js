c0bf19c7b176047b874ee83a3452797a88c230e4
Semou <semou.ach@gmail.com>
2021-08-05 17:27:34 +0200

    fin de journée

--- FRONTEND/js/script.js
+++ FRONTEND/js/script.js
@@ -1,3 +1,16 @@
-function getAllCountries() {
-    
-}
+
+    $("#btn").click(function getALLCountries(){
+        $.ajax({
+            url : "http://localhost:8000/all",
+            success: function(data, statuts, response){
+            $("#Pays").html(data[1].name)
+            }
+        })
+    })
+
+
+
+
+
+
+
